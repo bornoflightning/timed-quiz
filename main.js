240b424d309b2eac438820d981c72ccbc64b22a3
bornoflightning <victor.escarcega@hotmail.com>
2022-12-10 13:00:54 -0700

    designed front page

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 var questions = [
-    
+    {
+        question: "How do you log information to the console in javascript?",
+        anwer1: "using the 'print()' function",
+        answer2: "using 'System.out.println();' ",
+        answer3: "using console.log();",
+        correctAnswer: "using console.log();"
+    },
+
+    {
+        question: "How do you declare a variable named 'number' equal to 5 in javascript"
+        anwer1: "int number = 5",
+        answer2: "let number = 5",
+        answer3: "number = 5",
+        correctAnswer: "let number = 5"
+    },
+
+    {
+        question: "How do you declare a function in Javascript?"
+        anwer1: ""
+        answer2:
+        answer3:
+        correctAnswer:
+    },
+
+    {
+        question:
+        anwer1:
+        answer2:
+        answer3:
+        correctAnswer:
+    },
+
+    {
+        question:
+        anwer1:
+        answer2:
+        answer3:
+        correctAnswer:
+    }
 ]
